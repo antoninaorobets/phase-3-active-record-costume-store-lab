75fef66aac5940365d2b7c2eb2fe415fb9ecd9a9
antoninaorobets <pani.antonina.don@gmail.com>
2022-05-14 20:24:36 -0400

    commit tree 0d502ba54e760fdb63d7615a548b7d28b2641c34

---
+++ db/migrate/20220515002305_add_opening_time_closing_time_to_costumes.rb
@@ -0,0 +1,6 @@
+class AddOpeningTimeClosingTimeToCostumes < ActiveRecord::Migration[6.1]
+  def change
+    add_column :costume_stores, :opening_time, :datetime
+    add_column :costume_stores, :closing_time, :datetime
+  end
+end
